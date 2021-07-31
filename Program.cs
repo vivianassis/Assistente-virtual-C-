d626b9f95f37de2f7e9e00907e2aaed462ecd3be
vivianassis <vivianassis11@gmail.com>
2021-07-31 15:42:34 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Como é seu nome?");
+var name = Console.ReadLine();
+var currentDate = DateTime.Now;
+Console.WriteLine($"{Environment.NewLine}Oi, {name}, como você tá hoje? Espero que bem! {Environment.NewLine}Eu sou Lila, sua assistente virtual! {Environment.NewLine}{Environment.NewLine}Hoje é dia {currentDate:d}. São {currentDate:t}h! {Environment.NewLine}Não se atrase, ok?{Environment.NewLine}{Environment.NewLine}Tenha um ótimo dia! :D");
+
+Console.Write($"{Environment.NewLine}Press any key to exit...");
+Console.ReadKey(true);
+        }
+    }
+}
